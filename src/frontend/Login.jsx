a7395c2ceba1cd0f2da0341de111e87db969b211
Horia1408 <horia.costina96@gmail.com>
2021-11-03 21:52:52 +0200

    Login page frontend

---
+++ src/frontend/Login.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+function Login() {
+    return (
+        <Box
+            component="form"
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="on"
+        >
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    margin="dense"
+                    helperText="Please enter your username"
+                    id="demo-helper-text-aligned"
+                    label="Username"
+                    defaultValue=""
+                    style={{ width: 425 }}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    margin="dense"
+                    helperText="Please enter your password"
+                    id="utlined-adornment-password"
+                    label="Password"
+                    defaultValue=""
+                    type="password"
+                    style={{ width: 425 }}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 1, mb: 1 }}
+                    style={{ width: 447 }}
+                >
+                    Log In
+                </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="outlined"
+                    sx={{ mt: 1, mb: 1 }}
+                    style={{ width: 447 }}
+                >
+                    Register
+                </Button>
+            </Grid>
+
+        </Box>
+    );
+}
+
+export default Login
